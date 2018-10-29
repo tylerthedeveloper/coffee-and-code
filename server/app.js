72a0aeba35dfebcc0581b6578091949d17f8e11a
Tyler Citrin <tcitrin@iu.edu>
2018-10-29 15:02:04 -0400

    hosted DB

--- server/app.js
+++ server/app.js
@@ -1,17 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { Pool } = require('pg')
+const dotenv = require('dotenv');
+dotenv.load();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// TODO: PUT IN CONFIG + ENV VARIABLE
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: true,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
-var app = express();
+
+
+const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +38,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/test', () => {
+    pool.query('SELECT NOW()', (err, result) => {
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+      console.log('in query')
+      console.log(result.rows)
+    })
+})
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
