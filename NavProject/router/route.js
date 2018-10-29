5e9d57bbf908e4615d72f7ce65fd4d82aa7bef39
Abhishek Kanike <abkanike@iu.edu>
2018-10-28 22:20:36 -0400

    Menu Drawer

--- NavProject/router/route.js
+++ NavProject/router/route.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Platform, StatusBar } from "react-native";
+import { Platform, StatusBar, Text } from "react-native";
 import {
   StackNavigator,
   TabNavigator,
@@ -7,11 +7,13 @@ import {
   createStackNavigator,
   createBottomTabNavigator,
   createSwitchNavigator,
+  DrawerNavigator
 } from "react-navigation";
 import { FontAwesome } from "react-native-vector-icons";
 
 import LogIn from "../screens/LogIn";
 import Home from "../screens/Home";
+import Menu from "../screens/Menu";
 
 export const SignedOut = createStackNavigator({
   LogIn: {
@@ -23,39 +25,86 @@ export const SignedOut = createStackNavigator({
   }
 });
 
-export const SignedIn = createBottomTabNavigator(
-  {
-    Home: {
-      screen: Home,
-      navigationOptions: {
-        tabBarLabel: "Home",
-        tabBarIcon: ({ tintColor }) => (
-          <FontAwesome name="home" size={30} color={tintColor} />
-        )
-      }
-    },
-  },
-  {
-    tabBarOptions: {
-      style: {
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-      }
+// drawer stack
+const DrawerStack = DrawerNavigator({
+  screen1: { screen: Home },
+  // screen2: { screen: Screen2 },
+  // screen3: { screen3een: Screen3 },
+}, {
+  gesturesEnabled: false,
+  contentComponent: Menu
+})
+
+const drawerButton = (navigation) => 
+  <Text
+    style={{padding: 5, color: 'white'}}
+    onPress={() => {
+        console.log('open drawer');
+        console.log(navigation.state);
+        navigation.toggleDrawer();
     }
-  }
+  }>Menu</Text>;
+
+export const DrawerNavigation = createStackNavigator(
+  {
+      DrawerStack: { screen: DrawerStack },
+    }, {
+          headerMode: 'float',
+          navigationOptions: ({navigation}) => ({
+            headerStyle: {backgroundColor: '#4C3E54'},
+            title: 'Welcome!!!',
+            headerTintColor: 'white',
+            gesturesEnabled: false,
+            headerLeft: drawerButton(navigation)
+          })
+ }
 );
+
+
+export const SignedIn = createStackNavigator(
+  {
+      DrawerStack: { screen: DrawerNavigation },
+      // Home: {
+      //   screen: Home,
+      //   navigationOptions: {
+      //     tabBarLabel: "Home",
+      //     tabBarIcon: ({ tintColor }) => (
+      //       <FontAwesome name="home" size={30} color={tintColor} />
+      //     )
+      //   }
+      // },
+    }, {
+        // tabBarOptions: {
+        //   style: {
+        //     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+        //   }
+        // },
+        //   headerMode: 'float',
+        //   navigationOptions: ({navigation}) => ({
+        //     headerStyle: {backgroundColor: '#4C3E54'},
+        //     title: 'Welcome!',
+        //     headerTintColor: 'white',
+        //     gesturesEnabled: false,
+        //     headerLeft: drawerButton(navigation)
+        //   })
+        
+ }
+);
+
 
 export const createRootNavigator = (signedIn = false) => {
   return createSwitchNavigator(
     {
       SignedIn: {
-        screen: SignedIn
+        screen: DrawerNavigation
       },
       SignedOut: {
         screen: SignedOut
       }
     },
     {
-      initialRouteName: signedIn ? "SignedIn" : "SignedOut"
+      initialRouteName: signedIn ? "SignedIn" : "SignedOut",
+      headerMode: 'none'
     }
   );
 };
