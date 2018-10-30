30b1cc06e31afbf2231a2fe6f344952cd6f6e036
nishchayagupta <nishchayagupta@gmail.com>
2018-10-30 19:41:22 -0400

    stage

--- NavProject/app/screens/LogIn.js
+++ NavProject/app/screens/LogIn.js
@@ -1,24 +1,23 @@
-import React from "react";
-import { View } from "react-native";
-import { Card, Button } from "react-native-elements";
+import React from 'react';
+import { View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
-const onSignIn = () =>
-    new Promise((res, rej) => {
-        // AsyncStorage.setItem(USER_KEY, "true");
-        res("signedIn");
-    });
+const onSignIn = () => new Promise((res, rej) => {
+  // AsyncStorage.setItem(USER_KEY, "true");
+  res('signedIn');
+});
 
 export default ({ navigation }) => (
-    <View style={{ paddingVertical: 20 }}>
-        <Card>
-            <Button
-                buttonStyle={{ marginTop: 20 }}
-                backgroundColor="#03A9F4"
-                title="Login with GitHub"
-                onPress={() => {
-                    onSignIn().then(() => navigation.navigate("SignedIn"));
-                }}
-            />
-        </Card>
-    </View>
+  <View style={{ paddingVertical: 20 }}>
+    <Card>
+      <Button
+        buttonStyle={{ marginTop: 20 }}
+        backgroundColor="#03A9F4"
+        title="Login with GitHub"
+        onPress={() => {
+          onSignIn().then(() => navigation.navigate('SignedIn'));
+        }}
+      />
+    </Card>
+  </View>
 );
