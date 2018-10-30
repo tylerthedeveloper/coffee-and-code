4f5c93f1d14d44a880550a155f82c3bc1ccd9ef1
nishchayagupta <nishchayagupta@gmail.com>
2018-10-30 19:40:25 -0400

    stage

--- NavProject/app/router/route.js
+++ NavProject/app/router/route.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Platform, StatusBar, Text } from "react-native";
 import {
-  StackNavigator,
-  TabNavigator,
-  SwitchNavigator,
-  createStackNavigator,
-  createBottomTabNavigator,
-  createSwitchNavigator,
-  createDrawerNavigator
+    StackNavigator,
+    TabNavigator,
+    SwitchNavigator,
+    createStackNavigator,
+    createBottomTabNavigator,
+    createSwitchNavigator,
+    createDrawerNavigator
 } from "react-navigation";
 import { FontAwesome } from "react-native-vector-icons";
 
@@ -16,65 +16,72 @@ import Home from "../screens/Home";
 import Menu from "../screens/Menu";
 
 export const SignedOut = createStackNavigator({
-  LogIn: {
-    screen: LogIn,
-    navigationOptions: {
-      title: "Log In",
-      // headerStyle
+    LogIn: {
+        screen: LogIn,
+        navigationOptions: {
+            title: "Log In"
+            // headerStyle
+        }
     }
-  }
 });
 
 // drawer stack
 // const DrawerStack = DrawerNavigator({
-const DrawerStack = createDrawerNavigator({
-  screen1: { screen: Home },
-  // screen2: { screen: Screen2 },
-  // screen3: { screen3een: Screen3 },
-}, {
-  gesturesEnabled: false,
-  contentComponent: Menu
-})
-
-const drawerButton = (navigation) => 
-  <Text
-    style={{padding: 5, color: 'white'}}
-    onPress={() => {
-        console.log('open drawer');
-        console.log(navigation.state);
-        navigation.toggleDrawer();
+const DrawerStack = createDrawerNavigator(
+    {
+        screen1: { screen: Home }
+        // screen2: { screen: Screen2 },
+        // screen3: { screen3een: Screen3 },
+    },
+    {
+        gesturesEnabled: false,
+        contentComponent: Menu
     }
-  }>Menu</Text>;
-
-export const DrawerNavigation = createStackNavigator(
-  {
-      DrawerStack: { screen: DrawerStack },
-    }, {
-          headerMode: 'float',
-          navigationOptions: ({navigation}) => ({
-            headerStyle: {backgroundColor: '#4C3E54'},
-            title: 'Welcome!!!',
-            headerTintColor: 'white',
-            gesturesEnabled: false,
-            headerLeft: drawerButton(navigation)
-          })
- }
 );
 
+const drawerButton = navigation => (
+    <Text
+        style={{ padding: 5, color: "white" }}
+        onPress={() => {
+            console.log("open drawer");
+            console.log(navigation.state);
+            navigation.toggleDrawer();
+        }}
+    >
+        Menu
+    </Text>
+);
+
+export const DrawerNavigation = createStackNavigator(
+    {
+        DrawerStack: { screen: DrawerStack }
+    },
+    {
+        headerMode: "float",
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: { backgroundColor: "#4C3E54" },
+            title: "Welcome!!!",
+            headerTintColor: "white",
+            gesturesEnabled: false,
+            headerLeft: drawerButton(navigation)
+        })
+    }
+);
 
 export const SignedIn = createStackNavigator(
-  {
-      DrawerStack: { screen: DrawerNavigation },
-      // Home: {
-      //   screen: Home,
-      //   navigationOptions: {
-      //     tabBarLabel: "Home",
-      //     tabBarIcon: ({ tintColor }) => (
-      //       <FontAwesome name="home" size={30} color={tintColor} />
-      //     )
-      //   }
-      // },
-    }, {
+    {
+        DrawerStack: { screen: DrawerNavigation }
+        // Home: {
+        //   screen: Home,
+        //   navigationOptions: {
+        //     tabBarLabel: "Home",
+        //     tabBarIcon: ({ tintColor }) => (
+        //       <FontAwesome name="home" size={30} color={tintColor} />
+        //     )
+        //   }
+        // },
+    },
+    {
         // tabBarOptions: {
         //   style: {
         //     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
@@ -88,24 +95,21 @@ export const SignedIn = createStackNavigator(
         //     gesturesEnabled: false,
         //     headerLeft: drawerButton(navigation)
         //   })
-        
- }
+    }
 );
 
-
-export const createRootNavigator = (signedIn = false) => {
-  return createSwitchNavigator(
-    {
-      SignedIn: {
-        screen: DrawerNavigation
-      },
-      SignedOut: {
-        screen: SignedOut
-      }
-    },
-    {
-      initialRouteName: signedIn ? "SignedIn" : "SignedOut",
-      headerMode: 'none'
-    }
-  );
-};
+export const createRootNavigator = (signedIn = false) =>
+    createSwitchNavigator(
+        {
+            SignedIn: {
+                screen: DrawerNavigation
+            },
+            SignedOut: {
+                screen: SignedOut
+            }
+        },
+        {
+            initialRouteName: signedIn ? "SignedIn" : "SignedOut",
+            headerMode: "none"
+        }
+    );
