789d5d3edbfb3779597c5363f0dbb8d81e205657
nishchayagupta <nishchayagupta@gmail.com>
2018-10-30 20:17:02 -0400

    added plop files

---
+++ NavProject/plop-templates/class-screen.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import {
+    ScrollView,
+    Text,
+    Linking,
+    View,
+    Alert,
+    Platform,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity
+} from "react-native";
+import { Card, Button } from "react-native-elements";
+import { Constants, Location, Permissions } from "expo";
+
+const data = [
+
+];
+
+export default class {{ pascalCase name}} extends Component<Props> {
+
+    constructor(props) {
+        this.state = {
+
+        };
+    }
+
+    componentWillMount() {
+
+    }
+
+
+    render() {
+        const {  } = this.props;
+        console.log(this.props);
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    button: {
+    },
+    buttonContainer: {
+    }
+});
