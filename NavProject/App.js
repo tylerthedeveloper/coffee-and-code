6f21e8154d9cd0a0154e2790059a1ebd8661b963
Tyler Citrin <tcitrin@indiana.edu>
2018-11-01 15:18:59 -0400

    pretty husky

--- NavProject/App.js
+++ NavProject/App.js
@@ -13,9 +13,6 @@ export default class App extends React.Component {
 
   componentDidMount() {
     
-    // isSignedIn()
-    //   .then(res => this.setState({ signedIn: res}))
-    //   .catch(err => alert("An error occurred"));
   }
 
   render() {
