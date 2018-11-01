8ff9ef50b94b02b6c1a077da076422a11e142b13
Tyler Citrin <tcitrin@indiana.edu>
2018-10-31 20:16:41 -0400

    dv2

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send({ title: 'Docker v1' });
+  res.send({ title: 'Docker v2' });
 });
 
 module.exports = router;
