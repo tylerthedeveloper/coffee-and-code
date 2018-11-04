154618fdec6a281f6c4261f2a962dec0af810897
Abhishek Kanike <abkanike@iu.edu>
2018-11-04 17:24:41 -0500

    temporarily removed pass on redis

--- server/redis-client.js
+++ server/redis-client.js
@@ -4,8 +4,9 @@ dotenv.load();
 
 const client = redis.createClient({
     host: 'redis_server',
-    port: 6379,
-    password: process.env.REDIS_PWD
+    port: 6379
+    // TODO: 
+    // password: process.env.REDIS_PWD
 });
 
 client.on('connect', function() {
