df095ba8740f95d87a6fe25e1d2aed37cd406c2d
Tyler Citrin <tcitrin@indiana.edu>
2018-11-05 18:20:36 -0500

    the final redis deployment op

--- server/redis-client.js
+++ server/redis-client.js
@@ -2,17 +2,16 @@ const redis = require('redis');
 const dotenv = require('dotenv');
 dotenv.load();
 
-// const client = redis.createClient({
-//     host: 'redis_server',
-//     port: 6379
-//     // TODO: 
-//     // password: process.env.REDIS_PWD
-// });
-
-const client = redis.createClient(6380, 'cac.redis.cache.windows.net', {
-    auth_pass: process.env.REDIS_CACHE_PWD,
-    tls: { servername: 'cac.redis.cache.windows.net' }
+const client = redis.createClient({
+    host: process.env.REDIS_URI,
+    port: 6379,
+    password: process.env.REDIS_PWD
 });
+
+// const client = redis.createClient(6380, 'cac.redis.cache.windows.net', {
+//     auth_pass: process.env.REDIS_CACHE_PWD,
+//     tls: { servername: 'cac.redis.cache.windows.net' }
+// });
 
 client.on('connect', function() {
     console.log('Redis client connected');
