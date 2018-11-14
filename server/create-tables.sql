c2a2f5b135c021470277aba071a97a7a937a089c
Tyler Citrin <tcitrin@iu.edu>
2018-11-14 18:06:49 -0500

    updated friend requests to contain picture url

--- server/create-tables.sql
+++ server/create-tables.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS USERS (
-    user_id serial PRIMARY KEY,
+    userID serial PRIMARY KEY,
     git_username VARCHAR (50) UNIQUE NOT NULL,
     name VARCHAR NOT NULL,
     bio TEXT,
+    picture_url VARCHAR,
     current_latitude FLOAT,
     current_longitude FLOAT,
     current_location GEOGRAPHY
 );
 
-CREATE TABLE IF NOT EXISTS REPOSITORIES (
+CREATE TABLE IF NOT EXISTS REPOS (
     repoID serial PRIMARY KEY,
     repo_name VARCHAR (50) UNIQUE NOT NULL,
     language VARCHAR (10) NOT NULL,
@@ -16,13 +17,19 @@ CREATE TABLE IF NOT EXISTS REPOSITORIES (
 	repo_url VARCHAR(50),
 	creation_date DATE,
 	forks_count integer,
-	stargazers_count integer
-	user_name VARCHAR(50),
+	stargazers_count integer,
+	user_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS FRIENDS (
+    friendID integer PRIMARY KEY,
+    gitusername_1 VARCHAR (75) NOT NULL,
+    gitusername_2 VARCHAR (75) NOT NULL
 );
 
 insert into Users values (1, 'tylerthedeveloper', 'tyler citrin', 'i like cats', 39.1653, 86.5264, null);
 insert into Users values (2, 'abi', 'abi shek', 'afbeggi', 39.1653, 86.5364, null);
 
 UPDATE users SET current_location = ST_POINT(current_latitude, current_longitude);
-select * from users where user_id <> 1 and ST_DWithin(current_location, ST_POINT(39.1653, 86.5264), 10000);
+select * from users where git_username <> 'tylerthedeveloper' and ST_DWithin(current_location, ST_POINT(39.1653, 86.5264), 10000);
 
--- server/routes/friend-requests.js
+++ server/routes/friend-requests.js
@@ -10,7 +10,7 @@ const redisClient = require("../redis-client").redisClient;
 router.get("/:git_username/received", function(req, res, next) {
     const git_username = req.params["git_username"];
     const lookup = `${git_username}-received`;
-    console.log("sent");
+    console.log("received");
     return redisClient.lrange(lookup, 0, -1, function(error, result) {
         if (error) {
             console.log(error);
@@ -25,6 +25,7 @@ router.get("/:git_username/received", function(req, res, next) {
  * get friend requests sent sent by username
  */
 router.get("/:git_username/sent", function(req, res, next) {
+    console.log("sent");
     const git_username = req.params["git_username"];
     const lookup = `${git_username}-sent`;
     return redisClient.lrange(lookup, 0, -1, function(error, result) {
@@ -32,20 +33,26 @@ router.get("/:git_username/sent", function(req, res, next) {
             console.log(error);
             throw error;
         }
-        console.log("GET result -> " + result);
+        result.map(data => {
+            let buff = new Buffer(data, "base64");
+            let text = buff.toString("ascii");
+            console.log(text);
+        });
         res.send({ result: result });
     });
 });
 
-router.post("/:git_username_from/:git_username_to", function(req, res, next) {
-    const git_username_from = req.params["git_username_from"];
-    const git_username_to = req.params["git_username_to"];
+router.post("/", function(req, res, next) {
+    const data = req.body.data;
+    console.log(data.fromUser);
+    const { git_username_from, picture_url_from } = data.fromUser;
+    const { git_username_to, picture_url_to } = data.toUser;
     const fromLookup = `${git_username_from}-sent`;
     const toLookup = `${git_username_to}-received`;
-    console.log("SET result -> ");
+    console.log(fromLookup, git_username_from);
     const multiClient = redisClient.multi();
-    multiClient.rpush(fromLookup, git_username_to);
-    multiClient.rpush(toLookup, git_username_from);
+    multiClient.rpush(fromLookup, `${git_username_to}:${picture_url_to}`);
+    multiClient.rpush(toLookup, `${git_username_from}:${picture_url_from}`);
     return multiClient.exec(function(error, result) {
         if (error) {
             console.log(error);
@@ -56,6 +63,7 @@ router.post("/:git_username_from/:git_username_to", function(req, res, next) {
     });
 });
 
+// TODO:
 /**
  * delete friend-request by ID
  */
