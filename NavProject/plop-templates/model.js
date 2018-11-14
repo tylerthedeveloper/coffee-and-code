fc5b92fc9440d90add5361ffd1bccd8a8bd9d7fe
Tyler Citrin <tcitrin@iu.edu>
2018-11-14 16:15:07 -0500

    cleaned branch files

--- NavProject/plop-templates/model.js
+++
@@ -1,9 +0,0 @@
-export class {{ pascalCase name }} {
-
-    {{ camelCase name }}ID: string;
-
-    constructor(obj?: any) {
-        this.{{ camelCase name }}ID = obj.{{ camelCase name }}ID;
-    }
-
-}
