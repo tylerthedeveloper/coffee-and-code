a5af0551dda34ca985bbf82c7eb431b963a678a6
Abhishek Kanike <abkanike@iu.edu>
2018-11-23 01:20:11 -0500

    Configure redis client with new DevOps

--- server/redis-client.js
+++ server/redis-client.js
@@ -4,16 +4,15 @@ dotenv.load();
 
 const client = redis.createClient({
     host: process.env.REDIS_HOST,
-    url: process.env.REDIS_URL,
-    port: 6379,
-    password: process.env.REDIS_PWD
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_CACHE_PWD,
 });
 
 // const client = redis.createClient(6380, 'cac.redis.cache.windows.net', {
 //     auth_pass: process.env.REDIS_CACHE_PWD,
 //     tls: { servername: 'cac.redis.cache.windows.net' }
 // });
-
+``
 client.on('connect', function() {
     console.log('Redis client connected');
 });
