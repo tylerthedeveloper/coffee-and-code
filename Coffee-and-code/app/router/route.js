4c1b1cc27a5b98960e6e73ae81797480f57655f4
nishchayagupta <nishchayagupta@gmail.com>
2018-12-08 15:09:44 -0500

    removed preference button from  home

--- Coffee-and-code/app/router/route.js
+++ Coffee-and-code/app/router/route.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Text, AsyncStorage } from "react-native";
 import {
-    createStackNavigator,
-    createSwitchNavigator,
-    createDrawerNavigator,
-    createMaterialTopTabNavigator,
-    createAppContainer
+  createStackNavigator,
+  createSwitchNavigator,
+  createDrawerNavigator,
+  createMaterialTopTabNavigator,
+  createAppContainer
 } from "react-navigation";
 
 import LogIn from "../screens/LogIn";
@@ -17,253 +17,231 @@ import FriendsPage from "../screens/FriendsPage";
 import RepoPage from "../screens/RepoPage";
 import ChatThreads from "../screens/ChatThreads";
 import ChatMessages from "../screens/ChatMessages";
-import Preference from "../screens/Preference";
 
 // TODO: Separate files
 
 const SignedOut = createStackNavigator({
-    LogIn: {
-        screen: LogIn,
-        navigationOptions: {
-            header: null
-        }
+  LogIn: {
+    screen: LogIn,
+    navigationOptions: {
+      header: null
     }
+  }
 });
 
 const FriendStackNavigator = createStackNavigator({
-    Friends: {
-        screen: FriendsPage
-    }
+  Friends: {
+    screen: FriendsPage
+  }
 });
 
 const ProfileTabNavigator = createMaterialTopTabNavigator({
-    Profile: {
-        screen: Profile,
-        navigationOptions: {
-            title: "Profile"
-        }
-    },
-    Friends: {
-        screen: FriendsPage,
-        navigationOptions: {
-            title: "Friends"
-        }
-    },
-    Repos: {
-        screen: RepoPage,
-        navigationOptions: {
-            title: "Repositories"
-        }
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      title: "Profile"
     }
+  },
+  Friends: {
+    screen: FriendsPage,
+    navigationOptions: {
+      title: "Friends"
+    }
+  },
+  Repos: {
+    screen: RepoPage,
+    navigationOptions: {
+      title: "Repositories"
+    }
+  }
 });
 
 const ChatStackNavigator = createStackNavigator({
-    Chat: {
-        screen: ChatThreads,
-        navigationOptions: {
-            title: "Chats"
-        }
-    },
-    Messages: {
-        screen: ChatMessages,
-        navigationOptions: ({ navigation }) => ({
-            // headerStyle: { backgroundColor: "#4C3E54" },
-            // set height
-            title: `${navigation.state.params.title}`
-        })
+  Chat: {
+    screen: ChatThreads,
+    navigationOptions: {
+      title: "Chats"
     }
+  },
+  Messages: {
+    screen: ChatMessages,
+    navigationOptions: ({ navigation }) => ({
+      // headerStyle: { backgroundColor: "#4C3E54" },
+      // set height
+      title: `${navigation.state.params.title}`
+    })
+  }
 });
 
 const ScreenStackNavigator = createStackNavigator(
-    {
-        Home: {
-            screen: Home,
-            navigationOptions: {
-                title: "Home"
-            }
-        },
-        List: {
-            screen: List,
-            navigationOptions: {
-                title: "List"
-            }
-        },
-        Chat: {
-            screen: ChatStackNavigator,
-            navigationOptions: {
-                title: "Chat"
-            }
-        },
-        Profile: {
-            screen: ProfileTabNavigator,
-            navigationOptions: ({ navigation }) => {
-                const git_username = navigation.getParam("git_username");
-                const current_user = navigation.getParam("current_user");
-                return {
-                    title: git_username || "Me",
-                    current_user: current_user || ""
-                };
-            }
-        }
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        title: "Home"
+      }
     },
-    {
-        headerMode: "float",
-        navigationOptions: ({ navigation }) => ({
-            headerStyle: { backgroundColor: "#4C3E54" },
-            title: navigation.routeName || "Coffee-and-Code",
-            headerTintColor: "white",
-            gesturesEnabled: false,
-            headerLeft: drawerButton(navigation),
-            headerRight: prefButton(navigation),
-            git_username: navigation.getParam("git_username") || ""
-            // current_user: current_user || ""
-        })
+    List: {
+      screen: List,
+      navigationOptions: {
+        title: "List"
+      }
+    },
+    Chat: {
+      screen: ChatStackNavigator,
+      navigationOptions: {
+        title: "Chat"
+      }
+    },
+    Profile: {
+      screen: ProfileTabNavigator,
+      navigationOptions: ({ navigation }) => {
+        const git_username = navigation.getParam("git_username");
+        const current_user = navigation.getParam("current_user");
+        return {
+          title: git_username || "Me",
+          current_user: current_user || ""
+        };
+      }
     }
+  },
+  {
+    headerMode: "float",
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: { backgroundColor: "#4C3E54" },
+      title: navigation.routeName || "Coffee-and-Code",
+      headerTintColor: "white",
+      gesturesEnabled: false,
+      headerLeft: drawerButton(navigation),
+      git_username: navigation.getParam("git_username") || ""
+      // current_user: current_user || ""
+    })
+  }
 );
 
 const DrawerStack = createDrawerNavigator(
-    { ScreenStackNavigator },
-    {
-        gesturesEnabled: false,
-        contentComponent: Menu,
-        navigationOptions: ({ navigation }) => {
-            const git_username = navigation.getParam("git_username");
-            const current_user = navigation.getParam("current_user");
-            // console.log("navigation1", git_username);
-            return {
-                git_username: git_username || "Me",
-                current_user: current_user || ""
-            };
-        }
+  { ScreenStackNavigator },
+  {
+    gesturesEnabled: false,
+    contentComponent: Menu,
+    navigationOptions: ({ navigation }) => {
+      const git_username = navigation.getParam("git_username");
+      const current_user = navigation.getParam("current_user");
+      // console.log("navigation1", git_username);
+      return {
+        git_username: git_username || "Me",
+        current_user: current_user || ""
+      };
     }
+  }
 );
 
 const drawerButton = navigation => (
-    <Text
-        style={{ padding: 5, color: "white" }}
-        onPress={() => {
-            navigation.toggleDrawer();
-        }}
-    >
-        {/* TODO: Change to hamburger font awesome */}
-        Menu
-    </Text>
-);
-
-const PrefStackNavigator = createStackNavigator({
-    Preference: {
-        screen: Preference,
-        navigationOptions: {
-            title: "Preference"
-        }
-    }
-});
-const prefButton = navigation => (
-    <Text
-        style={{ padding: 5, color: "white", textAlign: "right" }}
-        // onPress={() => {
-        //     navigation.navigate("Screen1");
-        // }}
-    >
-        {/* TODO: Change to hamburger font awesome */}
-        Preference
-    </Text>
+  <Text
+    style={{ padding: 5, color: "white" }}
+    onPress={() => {
+      navigation.toggleDrawer();
+    }}
+  >
+    {/* TODO: Change to hamburger font awesome */}
+    Menu
+  </Text>
 );
 
 export const DrawerNavigation = createStackNavigator(
-    {
-        DrawerStack: {
-            screen: DrawerStack,
-            navigationOptions: ({ navigation }) => {
-                const git_username = navigation.getParam("git_username");
-                const current_user = navigation.getParam("current_user");
-                // console.log("navigation1", git_username);
-                return {
-                    git_username: git_username || "Me",
-                    current_user: current_user || ""
-                };
-            }
-        }
-    },
-    {
-        headerMode: "none",
-        navigationOptions: ({ navigation }) => {
-            const git_username = navigation.getParam("git_username");
-            const current_user = navigation.getParam("current_user");
-            // console.log("navigation1", git_username);
-            return {
-                git_username: git_username || "Me",
-                current_user: current_user || ""
-            };
-        }
+  {
+    DrawerStack: {
+      screen: DrawerStack,
+      navigationOptions: ({ navigation }) => {
+        const git_username = navigation.getParam("git_username");
+        const current_user = navigation.getParam("current_user");
+        // console.log("navigation1", git_username);
+        return {
+          git_username: git_username || "Me",
+          current_user: current_user || ""
+        };
+      }
     }
+  },
+  {
+    headerMode: "none",
+    navigationOptions: ({ navigation }) => {
+      const git_username = navigation.getParam("git_username");
+      const current_user = navigation.getParam("current_user");
+      // console.log("navigation1", git_username);
+      return {
+        git_username: git_username || "Me",
+        current_user: current_user || ""
+      };
+    }
+  }
 );
 
 export const SignedIn = createStackNavigator(
-    {
-        DrawerStack: {
-            screen: DrawerNavigation,
-            navigationOptions: ({ navigation }) => {
-                const git_username = navigation.getParam("git_username");
-                const current_user = navigation.getParam("current_user");
-                // console.log("navigation1", git_username);
-                return {
-                    git_username: git_username || "Me",
-                    current_user: current_user || ""
-                };
-            }
-        }
-    },
-    {
-        navigationOptions: ({ navigation }) => {
-            const git_username = navigation.getParam("git_username");
-            const current_user = navigation.getParam("current_user");
-            // console.log("navigation1", git_username);
-            return {
-                git_username: git_username || "Me",
-                current_user: current_user || ""
-            };
-        }
+  {
+    DrawerStack: {
+      screen: DrawerNavigation,
+      navigationOptions: ({ navigation }) => {
+        const git_username = navigation.getParam("git_username");
+        const current_user = navigation.getParam("current_user");
+        // console.log("navigation1", git_username);
+        return {
+          git_username: git_username || "Me",
+          current_user: current_user || ""
+        };
+      }
     }
+  },
+  {
+    navigationOptions: ({ navigation }) => {
+      const git_username = navigation.getParam("git_username");
+      const current_user = navigation.getParam("current_user");
+      // console.log("navigation1", git_username);
+      return {
+        git_username: git_username || "Me",
+        current_user: current_user || ""
+      };
+    }
+  }
 );
 
 export const createRootNavigator = (signedIn = false) =>
-    createSwitchNavigator(
-        {
-            SignedOut: {
-                screen: SignedOut
-            },
-            SignedIn: {
-                screen: DrawerNavigation,
-                navigationOptions: ({ navigation }) => {
-                    const git_username = navigation.getParam("git_username");
-                    const current_user = navigation.getParam("current_user");
-                    // console.log("navigation1", git_username);
-                    return {
-                        git_username: git_username || "Me",
-                        current_user: current_user || ""
-                    };
-                }
-            }
-        },
-        {
-            initialRouteName: signedIn ? "SignedIn" : "SignedOut",
-            // initialRouteName: async () => {
-            //     return await AsyncStorage.getItem("git_username")
-            //         .then(res => {
-            //             if (signedIn && res) return "SignedIn";
-            //             return "SignedOut";
-            //         });
-            // },
-            headerMode: "none",
-            navigationOptions: ({ navigation }) => {
-                const git_username = navigation.getParam("git_username");
-                const current_user = navigation.getParam("current_user");
-                // console.log("navigation1", git_username);
-                return {
-                    git_username: git_username || "Me",
-                    current_user: current_user || ""
-                };
-            }
+  createSwitchNavigator(
+    {
+      SignedOut: {
+        screen: SignedOut
+      },
+      SignedIn: {
+        screen: DrawerNavigation,
+        navigationOptions: ({ navigation }) => {
+          const git_username = navigation.getParam("git_username");
+          const current_user = navigation.getParam("current_user");
+          // console.log("navigation1", git_username);
+          return {
+            git_username: git_username || "Me",
+            current_user: current_user || ""
+          };
         }
-    );
+      }
+    },
+    {
+      initialRouteName: signedIn ? "SignedIn" : "SignedOut",
+      // initialRouteName: async () => {
+      //     return await AsyncStorage.getItem("git_username")
+      //         .then(res => {
+      //             if (signedIn && res) return "SignedIn";
+      //             return "SignedOut";
+      //         });
+      // },
+      headerMode: "none",
+      navigationOptions: ({ navigation }) => {
+        const git_username = navigation.getParam("git_username");
+        const current_user = navigation.getParam("current_user");
+        // console.log("navigation1", git_username);
+        return {
+          git_username: git_username || "Me",
+          current_user: current_user || ""
+        };
+      }
+    }
+  );
