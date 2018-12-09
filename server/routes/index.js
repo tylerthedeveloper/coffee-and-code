5b3c948263f1b93da706a3f388e14306e3e0623a
Abhishek Kanike <abkanike@iu.edu>
2018-12-09 13:23:56 -0500

    Expose port of test server in Dockerfile

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.send({ title: "Docker v7" });
+    res.send({ title: "Docker v8" });
 });
 
 module.exports = router;
